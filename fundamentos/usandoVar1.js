efda19df9ae41ad403f4c81c0a2fd97e151125b5
Gabriel Silva <90668697+Dakyon@users.noreply.github.com>
2021-09-30 20:42:28 -0300

    Add files via upload

---
+++ fundamentos/usandoVar1.js
@@ -0,0 +1,21 @@
+{
+    {
+        {
+            {
+                var sera = "Será???" 
+                console.log(sera)
+            }
+        }
+    }
+}
+
+console.log(sera)
+
+function teste() {
+    var local = 123
+    console.log(local)
+}
+
+teste()
+// O erro acontece porque não existe "local" fora da função
+console.log(local)
